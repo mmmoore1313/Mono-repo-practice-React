0456b052e23308c00b86d531c86e84f4b6b7b257
Matt Moore <Matt.M.Moore13@outlook.com>
2021-10-13 13:48:16 -0400

    Added the @monorepo-starter/button script to the packages/button/packages.json

---
+++ apps/next-app/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Button from "@monorepo-starter/button"
+
+const Index = () => {
+  return (
+    <div>
+      <Button isSelected onClick={() => alert("Hello!")}>
+        Hello World!
+      </Button>
+    </div>
+  )
+}
+
+export default Index
